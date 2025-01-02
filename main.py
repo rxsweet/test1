0b08f8f1f30f290c4116d61d5f3a96733ec1917b
rxsweet <105535100+rxsweet@users.noreply.github.com>
2025-01-02 11:58:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -338,7 +338,7 @@ def main():
     global mail
     num = 1  # 用于是否打印邀请码，只有第一个邮箱打印邀请码
     for email_user, email_pass in credentials:
-        """
+        
         try:
             mail = outlook.login(email_user, email_pass)
             logger.info(f"\n邮箱：{email_user}登录成功！")
@@ -346,7 +346,7 @@ def main():
             logger.error(f"\n邮箱：{email_user}\n" +
                          f"登录异常:{e}")
             continue
-        """
+        
         global invite_codes
         websites = read_websites(WEBSITES_FILE_PATH)
         # 遍历机场
